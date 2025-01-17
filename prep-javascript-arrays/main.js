376d3225ca74024e7a484b6606c61c64bf8821d2
Karen Bu <ktianbu@gmail.com>
2025-01-17 04:11:05 +0000

    Completed exercise prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array:', languages);
+
+const firstElement = languages[0];
+console.log('firstElement:', firstElement);
+
+const thirdElement = languages[2];
+console.log('thirdElement:', thirdElement);
+
+const length = languages.length;
+console.log('length of array:', length);
+
+const lastElement = languages[5];
+console.log('last element:', lastElement);
